ca84cec2d37f6909d750253561593a5a10df72af
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-12-30 00:41:28 -0600

    posts index view displays all posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -14,21 +14,40 @@ class PostsIndex extends Component {
     }
 
 
+    renderPosts(){
+        return this.props.posts.map((post)=>{
+            return (
+                <li className='list-group-item' key={post.id}>
+                    <span className='float-right'>{post.categories}</span>
+                    <strong>{post.title}</strong>
+                </li>
+            );
+        })
+    }
+
     render(){
         return(
             <div>
                 <div className='text-xs-right'>
                     <Link to='/posts/new' className='btn btn-primary'>Add a post</Link>
                 </div>
-                List of blog posts
+                <h3>Posts</h3>
+                <ul className='list-group'>
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
 }
 
 
+
+function mapStateToProps(state){
+    return {posts: state.posts.all};
+}
+
 // mapDispatchToProps is refactored in the export statement below to {fetchPosts:fetchPosts}
 // function mapDispatchToProps(dispatch){
 //     return bindActionCreators({fetchPosts}, dispatch);
 // }
-export default connect(null, {fetchPosts:fetchPosts})(PostsIndex);
+export default connect(mapStateToProps, {fetchPosts:fetchPosts})(PostsIndex);
